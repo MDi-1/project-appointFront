2c3d6b69d68bc99603132cf8db0656fb363d7b58
MDi <calendarzyk@gmail.com>
2022-06-15 10:24:48 +0200

    TimeFrame modification

--- src/main/java/com/example/appointfront/data/TimeFrame.java
+++ src/main/java/com/example/appointfront/data/TimeFrame.java
@@ -9,17 +9,8 @@ import lombok.Setter;
 @Setter
 public class TimeFrame {
     private Long id;
-    private String start0;
-    private String end0;
-    private String start1;
-    private String end1;
-    private String start2;
-    private String end2;
-    private String start3;
-    private String end3;
-    private String start4;
-    private String end4;
-    private String start5;
-    private String end5;
+    private String date;
+    private String timeStart;
+    private String timeEnd;
     private Long doctorId;
 }
